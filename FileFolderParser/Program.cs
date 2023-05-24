6b3bb4fab63e989627d6ff361e5df6b90174e20d
IM94 <gladiator.pw@gmail.com>
2023-05-25 00:39:45 +0300

    Was create project and added Program.cs with method which got list folers

---
+++ FileFolderParser/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace FileFolderParser
+{
+    class Program
+    {
+        const string DIRNAME = "D:\\A\\C Sharp";
+        static void Main(string[] args)
+        {
+            List<string> text = GetListFiles();
+            foreach (string file in text)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        static List<string> GetListFiles()
+        {
+            var directory = new DirectoryInfo(DIRNAME);
+            List<string> paths = new List<string>();
+            if (directory.Exists)
+            {
+                Console.WriteLine("Subdirectories: ");
+                DirectoryInfo[] dirs = directory.GetDirectories();
+                foreach (DirectoryInfo dir in dirs)
+                {
+                    paths.Add(dir.FullName);
+                }
+            }
+            return paths;
+        }
+    }
+}
