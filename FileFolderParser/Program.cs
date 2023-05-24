e7400a875c52d801a0efa72c1a4cbdffaccc01d6
IM94 <gladiator.pw@gmail.com>
2023-05-25 01:27:19 +0300

    Added the method that parses files in folder

--- FileFolderParser/Program.cs
+++ FileFolderParser/Program.cs
@@ -5,23 +5,38 @@ namespace FileFolderParser
 {
     class Program
     {
-        const string DIRNAME = "D:\\A\\C Sharp";
+        const string DIRNAME = "D:\\A\\C Sharp"; // Specifying the path to the dir
         static void Main(string[] args)
         {
-            List<string> text = GetListFiles();
-            foreach (string file in text)
+            List<string> folders = GetListFoldes();
+            foreach (string folder in folders)
             {
-                Console.WriteLine(file);
+                Console.WriteLine();
+                Console.Write("Folder: ");
+                Console.WriteLine(folder);
+
+                Console.WriteLine("Files: ");
+                List<string> files = GetListFiles(folder);
+                foreach (string file in files) // Parsing the files into folder
+                {
+                    Console.WriteLine($"      {file}");
+                }
+
             }
+
+            //List<string> files = GetListFiles("D:\\A\\C Sharp\\FileFolderParser"); // Test path
+            //foreach (string file in files)
+            //{
+            //    Console.WriteLine(file);
+            //}
         }
 
-        static List<string> GetListFiles()
+        static List<string> GetListFoldes() // return the list of the folders
         {
             var directory = new DirectoryInfo(DIRNAME);
             List<string> paths = new List<string>();
             if (directory.Exists)
             {
-                Console.WriteLine("Subdirectories: ");
                 DirectoryInfo[] dirs = directory.GetDirectories();
                 foreach (DirectoryInfo dir in dirs)
                 {
@@ -29,6 +44,20 @@ namespace FileFolderParser
                 }
             }
             return paths;
+        }
+        static List<string> GetListFiles(string dirName) // return the list of the files
+        {
+            var directory = new DirectoryInfo(dirName);
+            List<string> got_files = new List<string>();
+            string[] files = Directory.GetFiles(dirName);
+            if (directory.Exists)
+            {
+                foreach (string file in files)
+                {
+                    got_files.Add(file);
+                }
+            }
+            return got_files;
         }
     }
 }
